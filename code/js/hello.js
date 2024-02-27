d6e32fc71dbc8a110cdd2354111bdd4e6de956fc
franznkemaka <franz@rigle.co>
2024-02-27 10:30:41 +0100

    ✨ html + css, js

---
+++ code/js/hello.js
@@ -0,0 +1,12 @@
+let name = "Franz";
+let age = 21;
+let isStudent = false;
+console.log({ age, ageAsString: age.toString() });
+
+console.log(`Hello ${name}, du bist ${age} Jahre alt.`);
+
+// --- math
+
+console.log(`a + b = ${addition}`);
+console.log(`a - b = ${subtraction}`);
+console.log(`a * b = ${multiplication}`);
