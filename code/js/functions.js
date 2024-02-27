df855b6f4c587e6aa2ae14b53decfabff1305e0c
franznkemaka <franz@rigle.co>
2024-02-27 11:19:33 +0100

    ✨ functions

---
+++ code/js/functions.js
@@ -0,0 +1,28 @@
+function sayHello() {
+  console.log("Hallo");
+}
+
+function greet(person) {
+  return `Hello ${person.name}, du bist ${person.age} Jahre alt.`;
+}
+
+let greetMe = function (name) {
+  return "Hello: " + name;
+};
+
+let greetMeBetter = (name) => "Hello: " + name;
+
+let max = { name: "Max", age: 30 };
+let alice = { name: "Alice", age: 25 };
+
+let calculator = {
+  add: function (a, b) {
+    this.answer = a + b;
+    return this.answer;
+  },
+  subtract: function (a, b) {
+    return a - b;
+  },
+};
+
+console.log(calculator);
